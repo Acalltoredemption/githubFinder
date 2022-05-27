c68493915fecdd9604f00550b5658f10c7f8abac
rob oren <acalltoredemption@gmail.com>
2022-05-27 14:19:16 -0400

    implementing user search component

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,8 @@
+import spinner from './assets/loading.gif'
+export default function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+        <img className="text-center mx-auto" width={180} src={spinner} alt="loading..." />
+    </div>
+  )
+}
